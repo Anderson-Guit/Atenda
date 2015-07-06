2632cc2915ab02219d4e62e3f992409d763a3b97
Anderson Guit <anderson_guit@live.com>
2015-07-06 14:43:03 -0300

    Starting project Atenda

---
+++ Atenda/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Atenda.Startup))]
+namespace Atenda
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

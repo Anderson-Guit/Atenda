e0202bfb5a8dac75cf13b789f76f205507609df5
Anderson Guit <anderson_guit@live.com>
2015-09-18 14:21:35 -0300

    Implementation of classes on mobile platform (preview )

---
+++ Atenda.WP/Model/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atenda.WP.Model
+{
+    [Table(Name = "Cliente")]
+    class Cliente
+    {
+  
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int IdCliente { get; set; }
+        [Column]
+        public String Nome { get; set; }
+        [Column]
+        public String Telefone { get; set; }
+        [Column]
+        public String Endereco { get; set; }
+        [Column]
+        public String Bairro { get; set; }
+        [Column]
+        public String Cidade { get; set; }
+        [Column]
+        public String Estado { get; set; }
+        [Column]
+        public String Pais { get; set; }
+        [Column]
+        public String CPF { get; set; }
+
+    }
+}
---
+++ Atenda.WP/Model/Tecnico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atenda.WP.Model
+{
+    [Table(Name = "Tecnico")]
+    public class Tecnico
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int IdTecnico { get; set; }
+        [Column]
+        public String Nome { get; set; }
+        [Column]
+        public String Telefone { get; set; }
+        [Column]
+        public String Endereco { get; set; }
+        [Column]
+        public DateTime Adimicao { get; set; }
+
+    }
+}
